360e8b2422910430897e266402b6237a5502bab0
Erick-ViBe <erick.vicencio@e-vanhost.net>
2022-03-23 23:25:03 -0600

    feat: add GasStation model

--- gas_stations/models.py
+++ gas_stations/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class GasStation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    gas_station_id = models.CharField("Identificador de Gasolinera", max_length=24)
+    address = models.TextField("Direccion")
+    rfc = models.CharField("RFC", max_length=12)
+    regular_gasoline_price = models.FloatField("Precio de gasolina regular", null=True, blank=True)
+    premium_gasoline_price = models.FloatField("Precio de gasolina premium", null=True, blank=True)
+    diesel_price = models.FloatField("Precio de gasolina premium", null=True, blank=True)
+    longitude = models.CharField("Longitud", max_length=16)
+    latitude = models.CharField("Latitud", max_length=16)
+    zip_code = models.CharField("Codigo Postal", max_length=5)
+    company_name = models.CharField("Razon Social", max_length=255)
