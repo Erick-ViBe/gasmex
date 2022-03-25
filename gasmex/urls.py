e5196514cf5f7a6088cb33c8e7fcfd1e8649a72b
Erick-ViBe <erick.vicencio@e-vanhost.net>
2022-03-25 15:20:13 -0600

    feat: add dashboard app and statics

--- gasmex/urls.py
+++ gasmex/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gas_stations.urls'))
+    path('', include('gas_stations.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
